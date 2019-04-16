bbe5605183e4e884db26a393d6345fa51935bbcf
寺山弘晃 <hiro.nnsa724@gmail.com>
2019-04-16 12:56:54 +0900

    タスクのバリデーションを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,9 @@
 class Task < ApplicationRecord
   enum status: { waiting: 0, working: 1, completed: 2 }
   enum priority: { high: 1, middle: 2, low: 3, unselected: 10 }
+
+  validates :name, presence: true
+  validates :deadline, presence: true
+  validates :status, presence: true, inclusion: { in: Task.statuses.keys }
+  validates :priority, presence: true, inclusion: { in: Task.priorities.keys }
 end

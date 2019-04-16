ef2e2c5670c9aa3f31892fdb8b1096f41dfbf27a
寺山弘晃 <hiro.nnsa724@gmail.com>
2019-04-16 11:33:04 +0900

    一覧,詳細画面作成

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,10 @@
+class TasksController < ApplicationController
+  
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find_by(id: params[:id])
+  end
+end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+  enum status: { waiting: 0, working: 1, completed: 2 }
+  enum priority: { high: 1, middle: 2, low: 3, unselected: 10 }
 end

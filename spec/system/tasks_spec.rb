cbcc974a6b9fc7913ecf32300d55a21669f35a41
寺山弘晃 <hiro.nnsa724@gmail.com>
2019-04-18 11:54:50 +0900

    rspec でt()を使用できるように修正

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 include DateHelper
 
 RSpec.describe 'Tasks', type: :system do
-  let(:task) { FactoryBot.create(:task) }
+  let(:task) { create(:task) }
 
   scenario 'create new task' do
     visit new_task_path
-    fill_in I18n.t('activerecord.attributes.task.name'), with: 'nameテスト'
-    fill_in I18n.t('activerecord.attributes.task.description'), with: 'descriptionテスト'
-    fill_in I18n.t('activerecord.attributes.task.deadline'), with: Date.today.since(1.week)
-    select I18n.t('enums.task.status.waiting'), from: I18n.t('activerecord.attributes.task.status')
-    select I18n.t('enums.task.priority.middle'), from: I18n.t('activerecord.attributes.task.priority')
-    click_on I18n.t('components.buttons.create')
+    fill_in t('activerecord.attributes.task.name'), with: 'nameテスト'
+    fill_in t('activerecord.attributes.task.description'), with: 'descriptionテスト'
+    fill_in t('activerecord.attributes.task.deadline'), with: Date.today.since(1.week)
+    select t('enums.task.status.waiting'), from: t('activerecord.attributes.task.status')
+    select t('enums.task.priority.middle'), from: t('activerecord.attributes.task.priority')
+    click_on t('components.buttons.create')
     expect(page).to have_content 'タスクを作成しました！'
   end
 
@@ -20,14 +20,14 @@ RSpec.describe 'Tasks', type: :system do
     name = 'name更新テスト'
     description = 'description更新テスト'
     deadline = Date.today.since(1.week)
-    status = I18n.t('enums.task.status.working')
-    priority = I18n.t('enums.task.priority.high')
-    fill_in I18n.t('activerecord.attributes.task.name'), with: name
-    fill_in I18n.t('activerecord.attributes.task.description'), with: description
-    fill_in I18n.t('activerecord.attributes.task.deadline'), with: deadline
-    select status, from: I18n.t('activerecord.attributes.task.status')
-    select priority, from: I18n.t('activerecord.attributes.task.priority')
-    click_on I18n.t('components.buttons.create')
+    status = t('enums.task.status.working')
+    priority = t('enums.task.priority.high')
+    fill_in t('activerecord.attributes.task.name'), with: name
+    fill_in t('activerecord.attributes.task.description'), with: description
+    fill_in t('activerecord.attributes.task.deadline'), with: deadline
+    select status, from: t('activerecord.attributes.task.status')
+    select priority, from: t('activerecord.attributes.task.priority')
+    click_on t('components.buttons.create')
     expect(page).to have_content 'タスクを更新しました！'
     expect(page).to have_content name
     expect(page).to have_content description
@@ -40,7 +40,7 @@ RSpec.describe 'Tasks', type: :system do
     task
     visit tasks_path
     click_on '削除'
-    expect(page.driver.browser.switch_to.alert.text).to eq I18n.t('components.messages.confirmation.destroy')
+    expect(page.driver.browser.switch_to.alert.text).to eq t('components.messages.confirmation.destroy')
     expect {
       page.driver.browser.switch_to.alert.accept
       expect(page).to have_content 'タスクを削除しました！'
@@ -48,7 +48,7 @@ RSpec.describe 'Tasks', type: :system do
   end
 
   scenario 'show list order by created_at' do
-    5.times { |i| FactoryBot.create(:task, name: "#{i}-name") }
+    5.times { |i| create(:task, name: "#{i}-name") }
     visit tasks_path
     5.times { |i| expect(all('tbody tr')[i]).to have_content "#{4 - i}-name" }
   end

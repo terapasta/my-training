1ca0be170d3a293ba059fe691d2031d270cd2a66
寺山弘晃 <hiro.nnsa724@gmail.com>
2019-04-25 18:17:34 +0900

    ログイン情報をクッキーで保持する

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   has_secure_password validations: true
   has_many :tasks, dependent: :destroy
 
@@ -18,4 +19,26 @@ class User < ApplicationRecord
               with: VALID_PASSWORD_REGEX,
               message: I18n.t('errors.user.password')
             }, allow_blank: true
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.encrypt(token)
+    Digest::SHA256.hexdigest(token.to_s)
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.encrypt(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end

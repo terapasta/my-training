310d886fe6dd003adc252a82478fa67c8c98847c
寺山弘晃 <hiro.nnsa724@gmail.com>
2019-05-10 18:39:08 +0900

    グループのCRUD処理を追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
   skip_before_action :require_admin
+  before_action :set_group, only: [:show, :edit, :update, :destroy]
 
   def new
     @group = Group.new
@@ -19,6 +20,7 @@ class GroupsController < ApplicationController
   end
 
   def index
+    @groups = current_user.groups.page(params[:page])
   end
 
   def show
@@ -28,13 +30,32 @@ class GroupsController < ApplicationController
   end
 
   def update
+    if @group.update(group_params)
+      flash[:success] = t('messages.flash.success.update', model: t('activerecord.models.group'))
+      redirect_to @group
+    else
+      flash.now[:error] = t('messages.flash.error.update', model: t('activerecord.models.group'))
+      render :edit
+    end
   end
 
   def destroy
+    if @group.destroy
+      flash[:success] = t('messages.flash.success.destroy', model: t('activerecord.models.group'))
+      redirect_to groups_path
+    else
+      flash[:error] = t('messages.flash.error.destroy')
+      @groups = current_user.groups
+      render :index
+    end
   end
 
   private
     def group_params
       params.require(:group).permit(:name)
+    end
+
+    def set_group
+      @group = current_user.groups.find_by(id: params[:id])
     end
 end

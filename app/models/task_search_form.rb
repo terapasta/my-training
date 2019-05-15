d96af4f1a9236c24070518b944360f39294a788e
寺山弘晃 <hiro.nnsa724@gmail.com>
2019-05-15 10:47:00 +0900

    バグ修正

--- app/models/task_search_form.rb
+++ app/models/task_search_form.rb
@@ -8,7 +8,7 @@ class TaskSearchForm
     rel = rel.where_like_name(self.name) if self.name.present?
     rel = rel.where_eql_status(self.status) if self.status.present?
     rel = rel.where_eql_priority(self.priority) if self.priority.present?
-    rel = rel.where_eql_label_ids(self.label_ids) if self.label_ids.present?
+    rel = rel.where_eql_label_ids(self.label_ids) if self.label_ids.present? && self.label_ids.reject { |e| e.empty? }.present?
     rel = rel.where_eql_group_id(self.group_id) if self.group_id.present?
     rel
   end

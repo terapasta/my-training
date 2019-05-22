e7a0969fb1c4f63b809cb03fe1096a37cef48870
寺山弘晃 <hiro.nnsa724@gmail.com>
2019-05-22 10:17:25 +0900

    画像アップロード時にのプレビューを表示

--- app/javascript/packs/label_input.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue/dist/vue.esm'
-import TagsInput from '../components/TagsInput'
-
-document.addEventListener('DOMContentLoaded', () => {
-  new Vue({
-    el: '#task-form',
-    components: { TagsInput },
-  })
-})

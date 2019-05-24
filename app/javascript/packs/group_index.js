6b421d7fc522b85ff6ec225ca1b910283ff48f18
寺山弘晃 <hiro.nnsa724@gmail.com>
2019-05-24 18:08:56 +0900

    カレンダーを修正

---
+++ app/javascript/packs/group_index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue/dist/vue.esm'
+import VToggle from '../components/VToggle'
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    el: '#group-index',
+    components: { VToggle }
+  })
+})

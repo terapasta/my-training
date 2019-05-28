10aa59b2db791fa882c295e7fd066273e86754c2
寺山弘晃 <hiro.nnsa724@gmail.com>
2019-05-28 18:43:18 +0900

    config/deployを修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :branch, 'add-capistrano'
 set :deploy_to, '/var/www/rails/my-training'
 set :linked_files, fetch(:linked_files, []).push('config/settings.yml')
 set :linked_files, fetch(:linked_files, []).push('config/master.key')
-set :linked_dirs, fetch(:linked_dirs, [].push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'publick/system'))
+set :linked_dirs, fetch(:linked_dirs, [].push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system'))
 set :keep_releases, 5
 set :rbenv_ruby, '2.6.1'
 set :log_level, :debug

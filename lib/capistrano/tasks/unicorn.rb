5256c3868c44d1d12d2d6ef1a13181b87961caca
寺山弘晃 <hiro.nnsa724@gmail.com>
2019-05-28 18:37:39 +0900

    config/deployを修正

--- lib/capistrano/tasks/unicorn.rb
+++ lib/capistrano/tasks/unicorn.rb
@@ -1,6 +1,6 @@
 namespace :unicorn do
   task :environment do
-    set :incorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
+    set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
     set :unicorn_config, "#{current_path}/config/unicorn/production.rb"
   end
 end

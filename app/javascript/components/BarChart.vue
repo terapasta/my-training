5f19d46ecc903403303823e426663930910edfe7
寺山弘晃 <hiro.nnsa724@gmail.com>
2019-05-29 11:20:01 +0900

    ラベル使用頻度グラフを追加

---
+++ app/javascript/components/BarChart.vue
@@ -0,0 +1,66 @@
+<script>
+  import { Bar, mixins } from 'vue-chartjs'
+  import axios from '../shared/axios'
+
+  export default {
+    mixins: [Bar, mixins.reactiveData],
+    name: 'BarChart',
+    components: {
+      Bar
+    },
+    props: ["labels", "dataCollection"],
+    data() {
+      return {
+        chartData: {
+          labels: this.labels,
+          datasets: [
+            {
+              data: this.dataCollection,
+              backgroundColor: "hsl(204, 86%, 53%)"
+            }
+          ],
+        },
+        options: {                           
+          responsive: true,                 
+          legend: {                      
+              display: false             
+          },
+          title: {                         
+              display: true,             
+              fontSize: 18,                 
+              text: 'ラベル使用頻度'           
+          },
+          scales: {                     
+              yAxes: [{                  
+                  display: true,         
+                  scaleLabel: {            
+                      display: true,      
+                      labelString: 'ラベル使用回数', 
+                      fontSize: 18              
+                  },
+                  ticks: {                      
+                      min: 0,              
+                      fontSize: 18,            
+                      stepSize: 1          
+                  },
+              }],
+              xAxes: [{                     
+                  display: true,            
+                  scaleLabel: {               
+                      display: true,         
+                      labelString: 'ラベル種類', 
+                      fontSize: 18              
+                  },
+                  ticks: {
+                      fontSize: 18            
+                  },
+              }],
+          },
+        }
+      }
+    },
+    mounted() {
+      this.renderChart(this.chartData, this.options)
+    }
+  }
+</script>
